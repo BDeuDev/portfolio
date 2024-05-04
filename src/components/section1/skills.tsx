3df65ce75b37b8fa42a33227788f14e211bdee82
BDeuDev <bdeusich.dev@gmail.com>
2024-05-04 01:38:19 -0300

    Cambios

--- src/components/section1/skills.tsx
+++ src/components/section1/skills.tsx
@@ -14,7 +14,7 @@ interface BackendProps {
 const Skills:React.FC<BackendProps> = ({active,reference,image,text,title,index}) => {
 
     return (
-        <div className={`flex flex-col   bg-[#111c30]  my-[2.5%] mx-[10%] h-auto w-auto  rounded-lg shadow-xl  transition transform ease-in-out duration-700 ${active ? 'opacity-100 translate-x-0':'opacity-0 scale-[5%]'}`} ref={reference}>
+        <div className={`flex flex-col   bg-[#111c30]  my-[2.5%] mx-[15%] h-auto w-auto  rounded-lg shadow-xl  transition transform ease-in-out duration-700 ${active ? 'opacity-100 translate-x-0':'opacity-0 scale-[5%]'}`} ref={reference}>
                 <div className="w-full h-full  flex flex-col justify-center items-center ">
                     <h1 className="text-white text-3xl p-3 mt-3 cursor-pointer  transition transform ease-in-out duration-300 ">
                         {title[index]}
@@ -24,7 +24,7 @@ const Skills:React.FC<BackendProps> = ({active,reference,image,text,title,index}
                    
                          {image.map((images,index)=>(
                             <div key={index + 1} className="my-5 mx-5 cursor-pointer hover:scale-100 transition transform ease-in-out duration-300 scale-90">
-                            <div className={`w-[65px] h-[65px] my-5 rounded-2xl mx-5 bg-[#0a101b]`} >
+                            <div className={`w-[65px] h-[65px] my-5 rounded-2xl mx-5 `} >
                                 <img src={images} alt="" className={`object-cover w-full h-auto rounded-2xl`} />
                                 
                             </div>
